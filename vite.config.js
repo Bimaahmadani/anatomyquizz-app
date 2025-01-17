0670d01e0dd9f210823358924cc862530733d487
Bimaahmadani <bimaahmadani@gmail.com>
2025-01-17 15:18:46 +0700

    update vite.config.js, app.sevice.provider

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        [vercel()],
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
     build: {
-        outDir: 'public/',
         manifest: true,
       },
+    
 });
