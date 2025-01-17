9f08a19bd2f217ab1cd13d46186ebe95e8b2606f
Bimaahmadani <bimaahmadani@gmail.com>
2025-01-17 13:11:40 +0700

    update vercel.json, add api folder

--- public/index.php
+++ public/index.php
@@ -4,12 +4,8 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Tambahkan log untuk memantau permintaan
-error_log('Request started');
-
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    error_log('Application is in maintenance mode');
     require $maintenance;
 }
 
@@ -17,10 +13,5 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-try {
-    $app = require_once __DIR__.'/../bootstrap/app.php';
-    $app->handleRequest(Request::capture());
-} catch (Exception $e) {
-    // Log error jika terjadi
-    error_log('Error occurred: ' . $e->getMessage());
-}
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
